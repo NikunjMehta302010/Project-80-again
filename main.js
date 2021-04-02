a465895f99249de58c6c28922fa42744cb89673f
NikunjMehta302010 <76863868+NikunjMehta302010@users.noreply.github.com>
2021-04-02 14:00:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function getParagraph1()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("paral_input_box_1" + i).value);
+    }
+    document.getElementById("showParagraph1").innerHTML = inputs.join(", ");
+}
+
+function getParagraph2()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("input_box_1" + i).value);
+    }
+    document.getElementById("showParagraph2").innerHTML = inputs.join(", ");
+}
+
